60d595e5af9195e3635a4dfa1936b6c0dad218f2
Vitaliy Ya <vytaliy@live.ru>
2019-06-25 18:15:28 +0300

    Modified files as in the task 4.2-7

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,13 @@
 from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.chrome.webdriver import WebDriver
+# from selenium.webdriver.remote.webdriver import WebDriver
+# from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class BasePage (object):
     def __init__(self, browser, url, timeout = 10):
-        self.browser = browser
+        # self.browser = browser
+        self.browser: WebDriver = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
