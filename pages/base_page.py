ff5255515a00c605f860004011a2ef766ebf4780
Vitaliy Ya <vytaliy@live.ru>
2019-06-25 23:22:38 +0300

    Added method solve_quiz_and_get_code in class BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 # from selenium.webdriver.chrome.webdriver import WebDriver
 # from selenium.webdriver.remote.webdriver import WebDriver
 # from selenium.webdriver.firefox.webdriver import WebDriver
@@ -20,3 +21,16 @@ class BasePage (object):
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
